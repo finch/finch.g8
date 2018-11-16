ecfc8873e3e0db898576bdf008b526d2d3bcabee
Vladimir Kostyukov <vladimir.v.kostyukov@gmail.com>
2018-11-16 10:20:08 -0800

    Add JSON

--- src/main/g8/src/main/scala/$package__packaged$/Main.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Main.scala
@@ -1,23 +1,34 @@
 package $package$
 
 import cats.effect.IO
-import com.twitter.finagle.Http
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Await
 import io.finch._
 import io.finch.catsEffect._
+import io.finch.circe._
+import io.circe.generic.auto._
 
 object Main extends App {
 
-  def helloWorld: Endpoint[IO, String] = get("hello") {
-    Ok("Hello, World!")
+  case class Message(hello: String)
+
+  def healthcheck: Endpoint[IO, String] = get(pathEmpty) {
+    Ok("OK")
   }
 
-  def hello: Endpoint[IO, String] = get("hello" :: path[String]) { s: String =>
-    Ok("Hello, " + s + "!")
+  def helloWorld: Endpoint[IO, Message] = get("hello") {
+    Ok(Message("World"))
   }
 
-  Await.ready(
-    Http.server
-      .serve(":8081", (hello :+: helloWorld).toServiceAs[Text.Plain])
-  )
+  def hello: Endpoint[IO, Message] = get("hello" :: path[String]) { s: String =>
+    Ok(Message(s))
+  }
+
+  def service: Service[Request, Response] = Bootstrap
+    .serve[Text.Plain](healthcheck)
+    .serve[Application.Json](helloWorld :+: hello)
+    .toService
+
+  Await.ready(Http.server.serve(":8081", service))
 }
